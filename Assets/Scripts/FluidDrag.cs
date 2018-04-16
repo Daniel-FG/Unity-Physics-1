29b56ff09a907ed3ad4606abb5cba9f0099e6bc2
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-04-16 17:35:57 +0800

    Added UniversalGravitation.cs and modified PhysicsEngine.cs

---
+++ Assets/Scripts/FluidDrag.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FluidDrag : MonoBehaviour
+{
+    [Range (1f, 2f)]
+    public float velocityExponent;  //速度的次方  介於1次與2次
+
+    public float dragConstant;  //流體阻力常數
+
+    private PhysicsEngine physicsEngine;
+
+	void Start ()
+    {
+        physicsEngine = GetComponent<PhysicsEngine>();
+	}
+    private void FixedUpdate()
+    {
+        Vector3 velocityVector = physicsEngine.velocityVector;
+        float speed = velocityVector.magnitude;
+        float dragSize = CalculateDrag(speed);
+        Vector3 dragVector = dragSize * -velocityVector.normalized;
+        physicsEngine.AddForce(dragVector);
+    }
+
+    float CalculateDrag(float speed)
+    {
+        return dragConstant * Mathf.Pow(speed, velocityExponent);
+    }
+}
---
+++ Assets/Scripts/UniversalGravitation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalGravitation : MonoBehaviour
+{
+    private PhysicsEngine[] physicsEngineArray;
+    private const float gravityConstant = 6.673e-11f;
+    private void Start()
+    {
+        physicsEngineArray = FindObjectsOfType<PhysicsEngine>();
+    }
+    private void FixedUpdate()
+    {
+        CalculateGravity();
+    }
+    void CalculateGravity()
+    {
+        foreach (PhysicsEngine physicsEngineA in physicsEngineArray)
+        {
+            foreach (PhysicsEngine physicsEngineB in physicsEngineArray)
+            {
+                if (physicsEngineA != physicsEngineB && physicsEngineA != this)
+                {
+                    Debug.Log("Calculating force exerted on " + physicsEngineA.name + " due to the gravity of " + physicsEngineB.name);
+
+                    Vector3 distanceVector = physicsEngineA.transform.position - physicsEngineB.transform.position;
+                    float rSquared = Mathf.Pow(distanceVector.magnitude, 2f);
+                    float gravitationalForce = gravityConstant * physicsEngineA.mass * physicsEngineB.mass / rSquared;
+                    Vector3 gravityFeltVector = gravitationalForce * distanceVector.normalized;
+                    physicsEngineA.AddForce(-gravityFeltVector);
+                }
+            }
+        }
+    }
+}
