d350264915406b7c7607d97fbbdf0aed86ebd401
DANIEL-FG\o9708 <o97082@yahoo.com.tw>
2018-04-19 11:50:39 +0800

    Added MagnusEffect.cs

---
+++ Assets/Scripts/MagnusEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//馬格努斯效應
+//球在一邊往一個方向前進時與自身旋轉時會有馬格努斯力作用於本身  大小方向為角速度與速度正積
+public class MagnusEffect : MonoBehaviour
+{
+    public float magnusConstant = 1f;  //馬格努斯常數
+
+    private Rigidbody rigidBody;
+	void Start ()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+    {
+        rigidBody.AddForce(magnusConstant * Vector3.Cross(rigidBody.angularVelocity, rigidBody.velocity));
+	}
+}
